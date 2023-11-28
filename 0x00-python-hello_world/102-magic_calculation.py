63634c1db6952d416798764c2433d398089cf369
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-11-28 18:11:45 +0300

    calc edit

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,2 @@
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result += a  # LOAD_FAST 0 (a)
-    result = result ** b  # BINARY_POWER
-    result += b  # BINARY_ADD
-    return result  # RETURN_VALUE
+    return (98 + a ** b)
