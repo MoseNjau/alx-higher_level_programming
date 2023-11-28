16c48808f498c4e01abb7ddc88a3c2843a48cc41
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-11-29 00:07:00 +0300

    insert 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,47 +1,42 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * insert_node - insert a node
- * @head: head of list
- * @number: int
- * Return: NULL on fail
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be inserted
+ * Return: the address of the new node, or NULL if it failed
  */
-listint_t *insert_node(listint_t **head, const int number)
+listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *isCurrent, *tempo;
-	listint_t *node = (listint_t *) malloc(sizeof(listint_t));
+    listint_t *new_node, *current, *prev;
 
-	if (head == NULL)
-		return (NULL);
-	if (node == NULL)
-		return (NULL);
-	node->n = number;
-	node->next = NULL;
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	if (*head == NULL || number <= (*head)->n)
-	{
-		node->next = *head;
-		*head = node;
-		return (*head);
-	}
-	isCurrent = *head;
-	while (isCurrent->next != NULL)
-	{
-		if (isCurrent->n < number && isCurrent->next->n >= number)
-		{
-			tempo = isCurrent->next;
-			isCurrent->next = node;
-			node->next = tempo;
-		}
-		isCurrent = isCurrent->next;
-	}
+    new_node->n = number;
+    new_node->next = NULL;
 
-	if (number >= isCurrent->n)
-	{
-		tempo = isCurrent->next;
-		isCurrent->next = node;
-		node->next = tempo;
-	}
-	return (*head);
+    current = *head;
+    prev = NULL;
+
+    while (current != NULL && current->n < number)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (prev == NULL)
+    {
+        new_node->next = *head;
+        *head = new_node;
+    }
+    else
+    {
+        prev->next = new_node;
+        new_node->next = current;
+    }
+
+    return (new_node);
 }
