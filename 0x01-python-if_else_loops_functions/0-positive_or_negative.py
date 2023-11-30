f2d38f3ef1bee3b5fadae588fd5bd3eb6df9ed22
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-11-30 16:08:03 +0300

    check positive

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,11 +3,11 @@ import random
 
 number = random.randint(-10, 10)
 
-print(number)
+print(number, end='')
 
 if number > 0:
-    print("is positive")
+    print(" is positive")
 elif number == 0:
-    print("is zero")
+    print(" is zero")
 else:
-    print("is negative")
+    print(" is negative")
--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -17,7 +17,7 @@ listint_t *insert_node(listint_t **head, int number)
 
     new_node->n = number;
     new_node->next = NULL;
-
+	
     current = *head;
     prev = NULL;
 
