3462266f346bc4eb207d9297b110025d7357316b
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-12-20 00:25:34 +0300

    changes to file 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defines a class Square"""
+"""Defines a class Square"""
 
 
 class Square:
@@ -7,17 +7,22 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-    def __init__(self, size=0):
+    def __init__(self, s=0):
         """initializes the square
         Args:
             size (int): size of a side of the square
         Returns:
             None
         """
-        if type(size) is not int:
+        if not type(s) is int:
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        if s < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = s
+
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return self.__size * self.__size
