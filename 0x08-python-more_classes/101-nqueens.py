6b78af4f8022da333c39f1385703d5cb63c7efc1
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-01-10 21:39:17 +0300

    editted file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,24 +1,28 @@
 import sys
 
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        solution.append([i, board[i]])
+    print(solution)
+
 def is_safe(board, row, col):
     for i in range(row):
-        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
             return False
     return True
 
-def solve_n_queens(n, board, row, solutions):
+def solve_n_queens(n, board, row):
     if row == n:
-        solutions.append(board.copy())
+        print_solution(board)
         return
-    
+
     for col in range(n):
         if is_safe(board, row, col):
             board[row] = col
-            solve_n_queens(n, board, row + 1, solutions)
-
-def print_solutions(solutions):
-    for solution in solutions:
-        print([[i, solution[i]] for i in range(len(solution))])
+            solve_n_queens(n, board, row + 1)
 
 def main():
     if len(sys.argv) != 2:
@@ -35,10 +39,8 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = []
     board = [-1] * n
-    solve_n_queens(n, board, 0, solutions)
-    print_solutions(solutions)
+    solve_n_queens(n, board, 0)
 
 if __name__ == "__main__":
     main()
