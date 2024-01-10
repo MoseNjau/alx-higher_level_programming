898b629bd0eec09d042a743c71bff0bb12322fe8
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-01-10 23:00:13 +0300

    added file

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def add_attribute(obj, attribute, value):
+    """Adds a new attribute to an object if possible."""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
+
+add_attribute = __import__('101-add_attribute').add_attribute
+
+class MyClass():
+    pass
+
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
+
+try:
+    a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
