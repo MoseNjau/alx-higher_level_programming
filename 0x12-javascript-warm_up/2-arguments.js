c5c5ea86b0f2dbd903d676aa7c92c5b774d0ce0e
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-03-12 23:37:53 +0300

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const args = process.args.slice(2);
+const numArgs = args.length;
+
+if (numArgs === 0) {
+    console.log("No argument");
+  } else if (numArgs === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
